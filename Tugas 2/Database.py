06a764ba20b2cc367cc0d4abcc285b72b6dd4160
NoobBeast <84928001+SergioBavoBaparuha@users.noreply.github.com>
2023-03-20 23:16:10 +0700

    Add files via upload

---
+++ Tugas 2/Database.py
@@ -0,0 +1,3 @@
+# .streamlit/secrets.toml
+
+public_gsheets_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTUZmQ-1bsdQ97UZAvBPF4N2z3pIj1_6qSwYbMLz0fnVf7Xw2l1hBU5CXWGr_cCYs6Na28GIb0CaaB-/pub?output=csv"
